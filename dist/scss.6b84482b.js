fbe307b332e8dbef13aa92bed45dcf5dfd32741c
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-14 14:17:46 +0300

    Change the layout of the menu when using a desktop device

--- dist/scss.6b84482b.js
+++ dist/scss.6b84482b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\assets\\menu.svg":[["menu.f70022a6.svg","assets/menu.svg"],"assets/menu.svg"],"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
