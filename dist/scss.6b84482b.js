0115739d3f90cae997fd64821f805287eb663532
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-16 16:15:08 +0300

    Avoid oveflowing in the x axis

--- dist/scss.6b84482b.js
+++ dist/scss.6b84482b.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59333" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55763" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
